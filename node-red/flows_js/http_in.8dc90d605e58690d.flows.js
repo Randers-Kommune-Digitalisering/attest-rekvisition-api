9d9823d664524f5830003c3c3e22ed032d3b755c
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-02-24 15:18:57 +0100

    Add healthz endpoint

---
+++ node-red/flows_js/http_in.8dc90d605e58690d.flows.js
@@ -0,0 +1,19 @@
+const Node = {
+  "id": "8dc90d605e58690d",
+  "type": "http in",
+  "z": "c6de71541d12d2dd",
+  "name": "",
+  "url": "/healthz",
+  "method": "get",
+  "upload": false,
+  "swaggerDoc": "",
+  "x": 200,
+  "y": 400,
+  "wires": [
+    [
+      "86c54b1e434c880b"
+    ]
+  ]
+}
+
+module.exports = Node;
---
+++ node-red/flows_js/http_response.9cf741c0da05d029.flows.js
@@ -0,0 +1,13 @@
+const Node = {
+  "id": "9cf741c0da05d029",
+  "type": "http response",
+  "z": "c6de71541d12d2dd",
+  "name": "",
+  "statusCode": "200",
+  "headers": {},
+  "x": 560,
+  "y": 400,
+  "wires": []
+}
+
+module.exports = Node;
