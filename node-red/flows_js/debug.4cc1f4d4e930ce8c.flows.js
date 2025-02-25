9874dd70bd5ac20683ea75fe6c6af864b85fc887
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-02-25 14:36:19 +0100

    Add debugs

---
+++ node-red/flows_js/debug.4cc1f4d4e930ce8c.flows.js
@@ -0,0 +1,19 @@
+const Node = {
+  "id": "4cc1f4d4e930ce8c",
+  "type": "debug",
+  "z": "6268f9208c4f680c",
+  "name": "cpr debug",
+  "active": true,
+  "tosidebar": true,
+  "console": true,
+  "tostatus": false,
+  "complete": "true",
+  "targetType": "full",
+  "statusVal": "",
+  "statusType": "auto",
+  "x": 770,
+  "y": 120,
+  "wires": []
+}
+
+module.exports = Node;
---
+++ node-red/flows_js/debug.667d0a3c13de3c88.flows.js
@@ -0,0 +1,19 @@
+const Node = {
+  "id": "667d0a3c13de3c88",
+  "type": "debug",
+  "z": "6268f9208c4f680c",
+  "name": "cpr response debug",
+  "active": true,
+  "tosidebar": true,
+  "console": false,
+  "tostatus": false,
+  "complete": "true",
+  "targetType": "full",
+  "statusVal": "",
+  "statusType": "auto",
+  "x": 980,
+  "y": 120,
+  "wires": []
+}
+
+module.exports = Node;
