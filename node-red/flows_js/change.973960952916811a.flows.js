e87ce9b4561bef3f24605906b016ba49b0bf8ae7
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-03-25 15:04:53 +0100

    Add var to test

---
+++ node-red/flows_js/change.973960952916811a.flows.js
@@ -0,0 +1,36 @@
+const Node = {
+  "id": "973960952916811a",
+  "type": "change",
+  "z": "4882205932e440ad",
+  "name": "Afvist?",
+  "rules": [
+    {
+      "t": "set",
+      "p": "payload.erAfvist",
+      "pt": "msg",
+      "to": "dq in payload.brugerDQ = false ? 1 : 0",
+      "tot": "jsonata"
+    },
+    {
+      "t": "set",
+      "p": "afvistAt",
+      "pt": "msg",
+      "to": "erAfvist = 1 ? \"CURRENT_TIMESTAMP\" : \"\\\"0000-00-00 00:00:00\\\"\"",
+      "tot": "jsonata"
+    }
+  ],
+  "action": "",
+  "property": "",
+  "from": "",
+  "to": "",
+  "reg": false,
+  "x": 1130,
+  "y": 80,
+  "wires": [
+    [
+      "3b2ef6dd9b410f47"
+    ]
+  ]
+}
+
+module.exports = Node;
